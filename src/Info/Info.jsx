9408b9512637044a16ed77b37d60151a870d9e0e
partg952 <dankparth@gmail.com>
2021-06-02 23:26:17 +0530

    from pc

--- src/Info/Info.jsx
+++ src/Info/Info.jsx
@@ -3,17 +3,21 @@ import './Info.css'
 import axios from 'axios'
 import {useEffect,useState} from 'react'
 export default function Info() {
+    const [data,setData] = useState([])
     useEffect(() => {
+        console.clear();
        async function getData(){
            const id = sessionStorage.getItem('id')
            const response = await axios(`https://api.jikan.moe/v3/anime/${id}`)
            console.log(response.data)
+           setData([response.data])   
        }
        getData();
+      
    }, [])
     return (
         <div>
-            
+          
         </div>
     )
 }
