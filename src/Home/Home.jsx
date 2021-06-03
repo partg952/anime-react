2686c7de64a9f7cfa9e4d5d3752ae15795c16518
partg952 <dankparth@gmail.com>
2021-06-03 17:25:26 +0530

    completed homepage

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,28 +5,8 @@ import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button,Alert,Card,Row,Col } from 'react-bootstrap';
-function Home() {
+function Home({data}) {
 
-  const [data,
-    setData] = useState([])
-
-  useEffect(() => {
-    async function getData() {
-      let res = await axios("https://api.jikan.moe/v3/top/anime")
-      const {data} = res
-      console.log(data)
-      data
-        .top
-        .forEach(info => {
-	const {title,image_url} = info
-          setData(item => [...item,info])
-
-        })
-	console.log(data)
-    }
-
-    getData();
-  }, [])
 
   return (
     <>
@@ -38,12 +18,13 @@ function Home() {
         console.log(info.title)
         return (
             <Link to='/info'>
-            <Card variant="primary"  onClick={()=>{
+            <Card md bg='dark' 
+            border="light"  onClick={()=>{
                 console.log('clicked')
                 sessionStorage.setItem('id',info.mal_id)
             }}>
            
-           <Card.Img src={info.image_url} style={{height:'60%'},{width:'80%'}} />
+           <Card.Img src={info.image_url} style={{height:'60%'},{width:'100%'}} variant='top' />
            <p> {info.title} </p>
          </Card>
         </Link>
